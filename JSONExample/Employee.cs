fedc1f1d58ea69cf85b0ed68d6d01ece5ebbd565
hxchua00 <hxchua00@gmail.com>
2021-10-15 16:51:47 +0800

    Add project files.

---
+++ JSONExample/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace JSONExample
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public Guid ID { get; set; }
+        public string Designation { get; set; }
+        public string Address { get; set; }
+        public double Salary { get; set; }
+
+        //Must have default constructor for JSON, include it if have parameterised contructors
+        public Employee() { }
+    }
+}
---
+++ JSONExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JSONExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee()
+            {
+                ID = Guid.NewGuid(),
+                Name = "Han",
+                Address = "Platform 9 & 3/4",
+                Designation = "CEO",
+                Salary = 1000000000,
+            };
+            Employee emp1 = new Employee()
+            {
+                ID = Guid.NewGuid(),
+                Name = "HanXuan",
+                Address = "Diagon Alley, Three Broomsticks",
+                Designation = "CTO",
+                Salary = 1000000000,
+            };
+
+            List<Employee> empList = new List<Employee>();
+            empList.Add(emp);
+            empList.Add(emp1);
+
+            string empJson = JsonConvert.SerializeObject(empList);
+            //File.Create("Employee.Json");
+            File.WriteAllText("Employee.Json", empJson);
+            
+            //Deserialize same type of object, Eg SerializeObject(List<>) , DeserializeObject(List<>)
+            List<Employee> empTemp = JsonConvert.DeserializeObject<List<Employee>>(File.ReadAllText("Employee.Json"));
+
+            string empJson1 = JsonConvert.SerializeObject(empTemp);
+            File.WriteAllText("Employee.Json", empJson1);
+            Console.ReadLine();
+        }
+    }
+}
